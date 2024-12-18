31e4c297d41872397231c1f2bdade7952f8377fd
Fernando Sousa <nandocunhasousa@hotmail.com>
2024-12-18 00:20:36 +0000

    Changed menu logic

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,19 +1,68 @@
 using UnityEngine;
+using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class MenuController : MonoBehaviour
 {
-    public void PlayGame()
+    [SerializeField] private TMP_InputField inputField;
+    [SerializeField] private Button executeButton;
+    [SerializeField] private TMP_Text feedbackText;
+
+    private void Start()
     {
-        SceneManager.LoadScene("Level1");
-    }
-    public void ReturnToMainMenu()
-    {
-        SceneManager.LoadScene("MainMenu");
+        executeButton.onClick.AddListener(ProcessInput);
+
+        feedbackText.text = "Type a command: Play() or Quit()";
     }
 
-    public void QuitGame()
+    private void Update()
     {
-        Application.Quit();
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+            ProcessInput();
+
+        if (!inputField.isFocused)
+            RefocusInputField();
+    }
+
+    private void ProcessInput()
+    {
+        string input = inputField.text.ToLower();
+
+        if (input == "play()")
+        {
+            feedbackText.text = "Starting the game...";
+            StartCoroutine(StartCountdown());
+        }
+        else if (input == "quit()")
+        {
+            feedbackText.text = "Quitting the game...";
+            Application.Quit();
+        }
+        else feedbackText.text = "Invalid command. Try Play() or Quit()";
+
+        inputField.text = ""; // Clear input field for the next command
+        
+        RefocusInputField();
+    }
+
+    private IEnumerator StartCountdown()
+    {
+        float countdownTime = 1.5f;
+
+        while (countdownTime > 0)
+        {
+            countdownTime -= Time.deltaTime;
+            yield return null; // Wait for the next frame
+        }
+
+        SceneManager.LoadScene("Level1");
+    }
+
+    private void RefocusInputField()
+    {
+        inputField.Select();
+        inputField.ActivateInputField(); // Ensures input mode is active
     }
 }
